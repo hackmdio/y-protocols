eaea7492357ef29dc8915ede973d17356822646d
Max Wu <jackymaxj@gmail.com>
2025-01-17 22:31:56 +0800

    Update awareness.js

--- awareness.js
+++ awareness.js
@@ -56,29 +56,31 @@ export class Awareness extends Observable {
      * @type {Map<number, MetaClientState>}
      */
     this.meta = new Map()
-    this._checkInterval = /** @type {any} */ (setInterval(() => {
-      const now = time.getUnixTime()
-      if (this.getLocalState() !== null && (outdatedTimeout / 2 <= now - /** @type {{lastUpdated:number}} */ (this.meta.get(this.clientID)).lastUpdated)) {
-        // renew local clock
-        this.setLocalState(this.getLocalState())
-      }
-      /**
-       * @type {Array<number>}
-       */
-      const remove = []
-      this.meta.forEach((meta, clientid) => {
-        if (clientid !== this.clientID && outdatedTimeout <= now - meta.lastUpdated && this.states.has(clientid)) {
-          remove.push(clientid)
-        }
-      })
-      if (remove.length > 0) {
-        removeAwarenessStates(this, remove, 'timeout')
-      }
-    }, math.floor(outdatedTimeout / 10)))
+    this._checkInterval = /** @type {any} */ (setInterval(this.onInterval, math.floor(outdatedTimeout / 10)))
     doc.on('destroy', () => {
       this.destroy()
     })
     this.setLocalState({})
+  }
+
+  onInterval () {
+    const now = time.getUnixTime()
+    if (this.getLocalState() !== null && (outdatedTimeout / 2 <= now - /** @type {{lastUpdated:number}} */ (this.meta.get(this.clientID)).lastUpdated)) {
+      // renew local clock
+      this.setLocalState(this.getLocalState())
+    }
+    /**
+     * @type {Array<number>}
+     */
+    const remove = []
+    this.meta.forEach((meta, clientid) => {
+      if (clientid !== this.clientID && outdatedTimeout <= now - meta.lastUpdated && this.states.has(clientid)) {
+        remove.push(clientid)
+      }
+    })
+    if (remove.length > 0) {
+      removeAwarenessStates(this, remove, 'timeout')
+    }
   }
 
   destroy () {
